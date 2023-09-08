d830ffa70472c3c603a4af73405bcfb5e7890587
Ajim M <ajimsmaner@gmail.com>
2023-09-08 11:41:14 +0530

    start

---
+++ src/homework.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function homework() {
+  return (
+    <div>
+      <p>Ajim</p>
+    </div>
+  )
+}
+
+export default homework
--- src/react-homework.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function react-homework() {
-  return (
-    <div>react-homework</div>
-  )
-}
-
-export default react-homework
