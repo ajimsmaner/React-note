4967d18444e842910d47aab7ff61b314f6cd24a4
Ajim M <ajimsmaner@gmail.com>
2023-09-18 11:57:40 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import logo from './logo.svg';
 // import './App.css';
-import homework from './homework';
 
 function App() {
   return (
     <div className="App">
 
-      <homework />
       
     </div>
   );
